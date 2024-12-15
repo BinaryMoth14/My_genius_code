82729dafd5f7941010dad19954bc8df7c3191fb0
BinaryMoth14 <shadowkernel14@gmail.com>
2024-12-16 01:45:59 +0900

    Create alien_counter.py

---
+++ python_code/alien_counter.py
@@ -0,0 +1,11 @@
+# This code estimates the number of aliens visiting Earth based on the sky color.
+
+sky_color = "bluish-green"
+if sky_color == "red":
+    aliens = 1000
+elif sky_color == "bluish-green":
+    aliens = 42
+else:
+    aliens = 0
+
+print("Estimated number of aliens visiting Earth:", aliens)
