e85267e2169eabdf244e4d76d12ca5f6ca23871b
BinaryMoth14 <shadowkernel14@gmail.com>
2024-12-16 01:48:10 +0900

    Add files via upload

---
+++ python_code/carrot_investment_returns.py
@@ -0,0 +1,6 @@
+# This code calculates the investment return based on the number of carrots in your garden.
+
+carrots_in_garden = 27
+investment_return = carrots_in_garden ** 0.5  # Square root of carrot count
+
+print("Your fictional investment return:", investment_return)
