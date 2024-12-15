da297554989b50f49bf77275e547045e7bf09c6f
BinaryMoth14 <shadowkernel14@gmail.com>
2024-12-16 01:52:31 +0900

    Add files via upload

---
+++ python_code/marshmallow_thermometer.py
@@ -0,0 +1,6 @@
+# This code attempts to measure temperature by counting marshmallows in your cupboard.
+
+marshmallows = 10
+temperature = marshmallows / 2.5  # Random division
+
+print("Imagined temperature reading:", temperature)
